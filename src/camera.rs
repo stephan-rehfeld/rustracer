87ffc0b66eb64bd7c6aa6d33f398dd0624ce419a
Stephan Rehfeld <stephan.rehfeld@bht-berlin.de>
2023-07-01 00:19:52 +0200

    Capable to create first (but wrong) image.
    This commit contains the following changes:
    - Implementing perspective camera
    - Adding image crate as dependency
    - Performing fundamental raytracing between ray and plane
    - Creating output image, which is rotated by 90°

--- src/camera.rs
+++ src/camera.rs
@@ -7,14 +7,14 @@ pub struct Orthographic<T> {
     u: Vector3<T>,
     v: Vector3<T>,
     w: Vector3<T>,
+    scale: T,
     width: u32,
     height: u32,
-    scale: T,
     aspectRatio: T,
 }
 
 impl Orthographic<f32> {
-    pub fn new(e: Point3<f32>, g: Vector3<f32>, t: Vector3<f32>, width: u32, height: u32, scale: f32) -> Orthographic<f32> {
+    pub fn new(e: Point3<f32>, g: Vector3<f32>, t: Vector3<f32>, scale: f32, width: u32, height: u32) -> Orthographic<f32> {
         let w = -g.normalized();
         let u = Vector3::cross(t, w).normalized();
         let v = Vector3::cross(w, u);
@@ -24,7 +24,7 @@ impl Orthographic<f32> {
         Orthographic { e, u, v, w, width, height, scale, aspectRatio }
     }
 
-    pub fn ray_for(self, x: u32, y: u32) -> ParametricLine<f32> {
+    pub fn ray_for(&self, x: u32, y: u32) -> ParametricLine<f32> {
         let d = -self.w;
 
         let x = x as f32;
@@ -43,3 +43,38 @@ impl Orthographic<f32> {
     }
 }
 
+pub struct Perspective<T> {
+    e: Point3<T>,
+    u: Vector3<T>,
+    v: Vector3<T>,
+    w: Vector3<T>,
+    angle: T,
+    width: u32,
+    height: u32 
+}
+
+impl Perspective<f32> {
+    pub fn new(e: Point3<f32>, g: Vector3<f32>, t: Vector3<f32>, angle: f32, width: u32, height: u32) -> Perspective<f32> {
+        let w = -g.normalized();
+        let u = Vector3::cross(t, w).normalized();
+        let v = Vector3::cross(w, u);
+
+        Perspective { e, u, v, w, angle, width, height }
+    }
+
+    pub fn ray_for(&self, x: u32, y: u32 ) -> ParametricLine<f32> {
+        let o = self.e;
+
+        let x = x as f32;
+        let y = y as f32;
+
+        let width = self.width as f32;
+        let height = self.height as f32;
+
+        let r = -self.w * (height/2.0)/self.angle.tan() + (x - (width - 1.0)/2.0) * self.u + (y - (height-1.0)/2.0) * self.v;
+
+        let d = r.normalized();
+
+        ParametricLine::new(o, d)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,41 @@ use rustracer::math::Vector3;
 use rustracer::math::Point3;
 use rustracer::math::geometry::ParametricLine;
 use rustracer::math::geometry::Plane3;
+use rustracer::math::geometry::Intersect;
+use rustracer::camera;
 
 fn main() {
-    let origin = Point3::new( 0.0, 0.0, 0.0 );
-    let direction = Vector3::new( 0.0, 0.0, -1.0 ); 
+    let width = 640;
+    let height = 480;
 
-    let _ray = ParametricLine::new(origin, direction);
+    let plane = Plane3::new(
+        Point3::new(0.0f32, 0.0, 0.0),
+        Vector3::new(0.0f32, 1.0, 0.0)
+    );
 
-    let anchor = Point3::new( 0.0, -1.0, 0.0 );
-    let normal = Vector3::new( 0.0, 1.0, 0.0 );
+    let cam = camera::Perspective::new(
+        Point3::new(0.0, 1.0, 0.0),
+        Vector3::new(0.0, 0.0, -1.0),
+        Vector3::new(0.0, 1.0, 0.0),
+        90.0 / 180.0 * 3.1415,
+        width,
+        height
+    );
 
-    let _plane = Plane3::new(anchor, normal);
+    let mut imgbuf = image::ImageBuffer::new(width, height);
 
-    let _x = direction * direction;
+    for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
+        let ray = cam.ray_for(x, y);
+        let intersects = ray.intersect(plane);
+        
+        if intersects.len() != 0 && intersects[0] > 0.0 {
+            *pixel = image::Rgb([255u8, 0, 0]);
+        } else {
+            *pixel = image::Rgb([0u8, 0, 0]);
+        }
+    }
 
+    imgbuf.save("output.png").unwrap();
 
     println!("Hello, world!");
 }
