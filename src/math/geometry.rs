75ef537718f803b095a8eb454048aa6fa6333f6b
Stephan Rehfeld <stephan.rehfeld@bht-berlin.de>
2023-08-04 07:53:18 +0200

    Implement intersect test for AxisAlignedBox with ParametricLine

--- src/math/geometry.rs
+++ src/math/geometry.rs
@@ -474,12 +474,73 @@ mod tests {
                 assert_eq!(ray1.intersect(sphere), Vec::new());
                 assert_eq!(ray2.intersect(sphere), vec![3 as $type]);
                 assert_eq!(ray3.intersect(sphere), vec![1 as $type, 5 as $type]);
-
             }
         }
     }
 
     parametric_line_intersect_implicit_3_sphere! { f32, parametric_line_intersect_implicit_3_sphere_f32 }
     parametric_line_intersect_implicit_3_sphere! { f64, parametric_line_intersect_implicit_3_sphere_f64 }
+
+    macro_rules! new_axis_aligned_box3 {
+        ($type: ty, $name: ident) => {
+            #[test]
+            fn $name() {
+                let a = Point3::new( 1 as $type, 2 as $type, 3 as $type );
+                let b = Point3::new( 4 as $type, 5 as $type, 6 as $type );
+
+                let aab = AxisAlignedBox::new(a, b);
+
+                assert_eq!(aab.a, a);
+                assert_eq!(aab.b, b);
+            }
+        }
+    }
+
+    new_axis_aligned_box3! { u8, new_axis_aligned_box3_u8 }
+    new_axis_aligned_box3! { u16, new_axis_aligned_box3_u16 }
+    new_axis_aligned_box3! { u32, new_axis_aligned_box3_u32 }
+    new_axis_aligned_box3! { u64, new_axis_aligned_box3_u64 }
+    new_axis_aligned_box3! { u128, new_axis_aligned_box3_u128 }
+    new_axis_aligned_box3! { i8, new_axis_aligned_box3_i8 }
+    new_axis_aligned_box3! { i16, new_axis_aligned_box3_i16 }
+    new_axis_aligned_box3! { i32, new_axis_aligned_box3_i32 }
+    new_axis_aligned_box3! { i64, new_axis_aligned_box3_i64 }
+    new_axis_aligned_box3! { i128, new_axis_aligned_box3_i128 }
+    new_axis_aligned_box3! { f32, new_axis_aligned_box3_f32 }
+    new_axis_aligned_box3! { f64, new_axis_aligned_box3_f64 }
+
+    macro_rules! parametric_line_intersect_axis_aligned_box_3 {
+        ($type: ty, $name: ident) => {
+            #[test]
+            fn $name() {
+                let ray1 = ParametricLine::new(
+                    Point3::new(0 as $type, 0 as $type, 5 as $type),
+                    Vector3::new(0 as $type, 0 as $type, -1 as $type)
+                );
+                
+                let ray2 = ParametricLine::new(
+                    Point3::new(1 as $type, 1 as $type, 7 as $type),
+                    Vector3::new(0 as $type, 0 as $type, -1 as $type)
+                );
+                
+                let ray3 = ParametricLine::new(
+                    Point3::new(6 as $type, 0 as $type, 5 as $type),
+                    Vector3::new(0 as $type, 0 as $type, -1 as $type)
+                );
+
+                let aab = AxisAlignedBox::new(
+                    Point3::new(-2 as $type, -2 as $type, -2 as $type),
+                    Point3::new(2 as $type, 2 as $type, 2 as $type)
+                );
+
+                assert_eq!(ray1.intersect(aab), vec![3 as $type, 7 as $type]);
+                assert_eq!(ray2.intersect(aab), vec![5 as $type, 9 as $type]);
+                assert_eq!(ray3.intersect(aab), Vec::new());
+            }
+        }
+    }
+
+    parametric_line_intersect_axis_aligned_box_3! { f32, parametric_line_intersect_axis_aligned_box_3_f32 }
+    parametric_line_intersect_axis_aligned_box_3! { f64, parametric_line_intersect_axis_aligned_box_3_f64 }
 }
 
